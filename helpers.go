dc3d8c68ccf0e50edc2b4de228c061c380905631
Ta <mr.gronka@gmail.com>
2020-07-28 02:18:14 -0400

    updated exports

--- helpers.go
+++ helpers.go
@@ -13,47 +13,47 @@ var EmptyBytes = [16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 var OneDayMilli = 86400000
 var OneDaySecs = 86400
 
-func timeNowMilli() int64 {
+func TimeNowMilli() int64 {
 	return time.Now().UnixNano() / 1e6
 }
 
-func check(err error, msg string) {
+func Check(err error, msg string) {
 	if err != nil {
 		panic(errors.Wrap(err, msg))
 	}
 }
 
-func printBytes(bytes []byte) {
+func PrintBytes(bytes []byte) {
 	for _, n := range bytes {
 		fmt.Printf("%8b", n) // prints 1111111111111101
 	}
 	fmt.Printf("\n")
 }
 
-func logPanicf(i interface{}) { // 5
+func Panic(i interface{}) { // 5
 	log.Panic().Msg(fmt.Sprintf("%v", i))
 }
 
-func logFatalf(i interface{}) { // 4
+func Fatal(i interface{}) { // 4
 	log.Fatal().Msg(fmt.Sprintf("%v", i))
 }
 
-func logErrorf(i interface{}) { // 3
+func Error(i interface{}) { // 3
 	log.Error().Msg(fmt.Sprintf("%v", i))
 }
 
-func logWarnf(i interface{}) { // 2
+func Warn(i interface{}) { // 2
 	log.Warn().Msg(fmt.Sprintf("%v", i))
 }
 
-func logInfof(i interface{}) { // 1
+func Info(i interface{}) { // 1
 	log.Info().Msg(fmt.Sprintf("%v", i))
 }
 
-func logDebugf(i interface{}) { // 0
+func Debug(i interface{}) { // 0
 	log.Debug().Msg(fmt.Sprintf("%v", i))
 }
 
-func logTracef(i interface{}) { // -1
+func Trace(i interface{}) { // -1
 	log.Trace().Msg(fmt.Sprintf("%v", i))
 }
